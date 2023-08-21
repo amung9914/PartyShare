732f3c5bc1423b19aa53a48be64080b3ffca6658
amung9914 <137124338+amung9914@users.noreply.github.com>
2023-08-21 20:31:24 +0900

    파티게시판 및 신고테이블 업데이트

--- new_sql.sql
+++ new_sql.sql
@@ -145,11 +145,11 @@ CREATE TABLE report  -- -- 신고내역 --
     FOREIGN KEY (toMid) REFERENCES member(mId) ON UPDATE CASCADE
 );
 
-CREATE TABLE notice  -- -- 신고내역 --  
+CREATE TABLE notice  -- -- 알림 --  
 (
     noticeNum    	INT primary key auto_increment,  
     date  			 date NOT NULL,
-    context   		 TEXT  NOT NULL   -- 신고 내용 
+    context   		 TEXT  NOT NULL   
 );
 
 CREATE TABLE banMember -- -- 신고내역 --
@@ -307,3 +307,86 @@ CREATE TABLE IF NOT EXISTS freeBoardComment(
    FOREIGN KEY(mnick) REFERENCES member(mNick) ON UPDATE CASCADE,
    FOREIGN KEY(mid) REFERENCES member(mId) ON UPDATE CASCADE
 );
+
+
+-- 파티 게시판 수정
+drop table partyboard;
+
+CREATE TABLE partyboard(
+	bno INT PRIMARY KEY auto_increment, 	-- 게시글 번호
+    pnum INT NOT NULL, 						-- 파티 번호
+	title VARCHAR(200) NOT NULL,			-- 제목
+	content LONGTEXT NOT NULL,				-- 내용
+	writer VARCHAR(50) NOT NULL,			-- 작성자 이름
+    category VARCHAR(20) NOT NULL,			-- 카테고리
+	origin INT NULL DEFAULT 0,				-- 원본글 그룹 번호
+	depth INT NULL DEFAULT 0,				-- view 깊이 번호
+	seq INT NULL DEFAULT 0,					-- 답변글 정렬 순서
+	regdate TIMESTAMP NULL DEFAULT NOW(), 	-- 게시글 등록 시간
+	updatedate TIMESTAMP NULL DEFAULT now(),-- 게시글 수정 시간
+	viewcnt INT NULL DEFAULT 0,				-- 조회수
+	showboard VARCHAR(10) NULL DEFAULT 'y',	-- 게시글 삭제요청 여부
+	mnum INT NOT NULL,						-- 게시글 작성자 회원번호
+	reported char(1) default 'N',
+	CONSTRAINT fk_partyboard_mnum
+	FOREIGN KEY(mnum) REFERENCES member(mnum),
+    CONSTRAINT fk_partyboard_pnum
+	FOREIGN KEY(pnum) REFERENCES party(pnum)
+);
+
+-- 파티 게시판 댓글 테이블 생성
+
+CREATE TABLE partyboard_comment(
+	cno INT PRIMARY KEY AUTO_INCREMENT,			-- 댓글 번호
+	bno INT NOT NULL,							-- 댓글 작성 게시글 번호
+	pnum INT NOT NULL,							-- 파티 번호
+	commentText TEXT NOT NULL,					-- 댓글 내용
+	mnick VARCHAR(20) NOT NULL,					-- 작성자 닉네임
+	mid VARCHAR(20) NOT NULL,					-- 작성자 아이디
+	regdate TIMESTAMP NOT NULL DEFAULT now(),	-- 작성시간
+	updatedate TIMESTAMP NOT NULL DEFAULT now(),-- 수정시간
+	reported char(1) default 'N',
+	CONSTRAINT fk_partyboard_comment_bno FOREIGN KEY(bno) -- 참조무결성 추가
+	REFERENCES partyboard(bno) ON DELETE CASCADE,
+	CONSTRAINT fk_partyboard_comment_pnum		
+	FOREIGN KEY(pnum) REFERENCES party(pnum)ON DELETE CASCADE,
+	CONSTRAINT fk_partyboard_comment_mnick		
+	FOREIGN KEY(mnick) REFERENCES member(mnick)ON DELETE CASCADE,
+	CONSTRAINT fk_partyboard_comment_mid		
+	FOREIGN KEY(mid) REFERENCES member(mid)ON DELETE CASCADE,
+	INDEX(bno)									-- 인덱스 추가
+);
+
+-- 파티게시판 신고내역테이블 신규 생성
+CREATE TABLE partyboard_report  
+(
+    no    		INT primary key auto_increment,  
+    fromMid     VARCHAR(20)  NOT NULL,  
+    toMid 		VARCHAR(20)  NOT NULL,
+    date  		 TIMESTAMP default now(),
+    category   VARCHAR(20) NOT NULL,   -- 신고 카테고리 
+    context    TEXT  NOT NULL,   -- 신고 내용 
+    readed char(1) default 'N',
+    pnum INT, -- 파티번호
+    bno INT, -- 게시글 번호
+    cno INT, -- 댓글 번호
+	FOREIGN KEY (pnum) REFERENCES party(pnum)ON DELETE CASCADE,
+	FOREIGN KEY (bno) REFERENCES partyboard(bno)ON DELETE CASCADE,
+	FOREIGN KEY (cno) REFERENCES partyboard_comment(cno)ON DELETE CASCADE,
+	FOREIGN KEY (fromMid) REFERENCES member(mId) ON UPDATE CASCADE,
+    FOREIGN KEY (toMid) REFERENCES member(mId) ON UPDATE CASCADE
+);
+
+
+-- 신고 관련 수정
+ALTER TABLE report
+ADD COLUMN bno INT null,
+ADD FOREIGN KEY (bno) REFERENCES freeboard(bno);
+
+ALTER TABLE report
+ADD COLUMN cno INT,
+ADD FOREIGN KEY (cno) REFERENCES freeBoardComment(bno);
+
+alter table freeBoardComment add column reported char(1) default 'N';
+alter table freeBoard add column reported char(1) default 'N'; 
+
